e4e557d7333978e0de5cab2ad465e5e0cf274e86
= <veil_ctf@protonmail.com>
2021-08-22 11:17:17 +0200

    Add exceptions

--- pyserv/login.py
+++ pyserv/login.py
@@ -30,4 +30,4 @@ class IServ:
     def start():
         login_req = requests.post(url, headers={"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/74.0.3729.169 Safari/537.36"}, data={"_username": self.username, "_password": self.password})
         if not "displayName" in login_req.text:
-            raise LoginErr(login_req)
+            raise LoginErr
