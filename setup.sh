1028c3a3609b50400dd5062f23bfaf675604532c
= <veil_ctf@protonmail.com>
2021-08-25 12:14:14 +0200

    improved setup.sh

--- setup.sh
+++ setup.sh
@@ -6,4 +6,6 @@ if ! [ -x "$(command -v python3)" ]; then
 else
 	echo "Installing via pip..";
 	pip3 install . || python3 setup.py install;
+	echo "PyServ should be installed now. Running small test..";
+	python3 -c "from pyserv import login;iserv=login.IServ(\"username\", \"password\", \"https://google.com/\");print(\"\033[32mPyServ installed properly!\") if iserv.login() == False else print(\"\033[91mCould not install PyServ, please try again or open an issue on github.\")" || echo "Something broke, please open an issue on github.";
 fi
