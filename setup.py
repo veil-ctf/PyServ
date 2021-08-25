46c98c265fa8e6c258460e6215a8c3019d6835c2
= <veil_ctf@protonmail.com>
2021-08-25 20:19:57 +0200

    revert/fix

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setup(
     name='pyserv',
     version='1.0',
 	packages=["pyserv", "pyserv.extra", "pyserv.extra.exceptions"],
-    install_requires = [x.rstrip() for s in (open("requirements.txt", "r"))]
+    install_requires = [x.rstrip() for x in (open("requirements.txt", "r"))],
 	license='MIT',
     description='IServ API library',
     author='Veil',
